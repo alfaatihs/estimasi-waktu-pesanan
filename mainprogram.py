2f5392439b51d86e4593b73d7ef7b93d4ccdb5e3
alfaatihs <alfaatihs@student.uns.ac.id>
2022-06-09 04:51:35 +0700

    menupesan hampir selesai!

--- mainprogram.py
+++ mainprogram.py
@@ -1,7 +1,13 @@
 import csv
 from tabulate import tabulate
-import datetime
+import datetime as dt
 import time
+
+def sumcolumn(list,column):
+    total = 0
+    for row in range(len(list)):
+        total = total + list[row][column-1]
+    return total
 
 def listconversion(list,column,func):
     for i in range (len(list)):
@@ -58,47 +64,110 @@ def menuexit(): #konfirmasi exit (apang)
     """)
     pilihan = int(input("Yakin untuk Keluar?"))
     if pilihan == 1 :
-        menuawal()
+        main()
     elif pilihan == 2:
         quit()
     else:
         menuexit()
 
+def generatenopesan():
+    date = dt.date.now()
+    date = date.strftime("%Y%m%d")
+    no = no + 1
+    nopesan = date + str(no)
+    return nopesan
+    
+def kecamatan():
+    global ongkir
+    global lamakirim
+
+    try:
+        print("""
+        ==========================================
+                    Pilih Kecamatan
+        ==========================================
+        1. Laweyan
+        2. Serengan
+        3. Jebres
+        4. Banjarsari
+        5. Pasar Kliwon
+        """)
+            
+        kecamatan = int(input(">> "))
+        if kecamatan == 1:
+            ongkir = 6000
+            lamakirim = dt.timedelta(minutes=10) #INI BRP MENIT Y KIRA2?
+        elif kecamatan ==2:
+            ongkir = 10000
+            lamakirim = dt.timedelta(minutes=10) #INI BRP MENIT Y KIRA2?
+        elif kecamatan == 3:
+            ongkir = 8000
+            lamakirim = dt.timedelta(minutes=10) #INI BRP MENIT Y KIRA2?
+        elif kecamatan == 4:
+            ongkir = 9000
+            lamakirim = dt.timedelta(minutes=10) #INI BRP MENIT Y KIRA2?
+        elif kecamatan == 5:
+            ongkir = 7000
+            lamakirim = dt.timedelta(minutes=10) #INI BRP MENIT Y KIRA2?
+        else:
+            print("Input tidak valid, silahkan coba lagi")
+            kecamatan()
+    except ValueError:
+        print("Input tidak valid, silahkan coba lagi")
+        kecamatan()
+
+def timeygmana():
+    with open ('data.csv','r') as filedata:
+        readdata = csv.reader(filedata)
+        listdata = list(readdata)
+    try:
+        if listdata[len(listdata)-1][5]-timepesan < 0:
+            return len(listdata[len(listdata)-1][5])
+        else:
+            return timepesan
+    
+    except ValueError:
+        return timepesan
+
+#def receipt(): LANJUT KE SINI
+
+def writedatapesan():
+    with open ('data.csv','w') as filedata:
+        writedata = csv.writer(filedata)
+        writedata.writerow(datapesan)
+
+def waktu():
+    global timepesan
+
+    timepesan = dt.datetime.now()
+    lamabuat = dt.timedelta(minutes=5*sumcolumn(listpesanan,2)) #WAKTU BUAT 1 ROTI BAKAR BRP???
+    timejadi = timeygmana() + lamabuat
+    timesampai = timejadi + lamakirim
+    datapesan.append(timepesan)
+    datapesan.append(timejadi)
+    datapesan.append(timesampai)
+    writedatapesan()
+
 def inputdatadiri():
+    global datapesan
+
     print("""
     ==========================================
                 Masukkan Data Diri
     """)
     nama = input("Nama = ")
     notelp = input("No Telp = ")
-    print("""
-    ==========================================
-                Pilih Kecamatan
-    ==========================================
-    1. Laweyan
-    2. Serengan
-    3. Jebres
-    4. Banjarsari
-    5. Pasar Kliwon
-    """)
-    
-    kecamatan = int(input(">> "))
-    if kecamatan == 1:
-        ongkir = 6000
-    elif kecamatan ==2:
-        ongkir = 10000
-    elif kecamatan == 3:
-        ongkir = 8000
-    elif kecamatan == 4:
-        ongkir = 9000
-    elif kecamatan == 5:
-        ongkir = 7000
-    else:
-        print("Input tidak valid, silahkan coba lagi")
-        inputdatadiri()
-    
+    kecamatan()
     print("Masukkan alamat lengkap =")
     alamat = input(">> ")
+    nopesan = generatenopesan()
+
+    datapesan = []
+    datapesan.append(nopesan)
+    datapesan.append(nama)
+    datapesan.append(notelp)
+    datapesan.append(alamat)
+    waktu()
 
 def konfirmasi():
     try:    
@@ -130,10 +199,7 @@ def konfirmasi():
 
 def hitungkonfirmasi():
     global totalharga
-    totalharga = 0
-    for i in range(len(listpesanan)):
-        totalharga = totalharga + listpesanan[i][2]
-    
+    totalharga = sumcolumn(listpesanan,3)    
     totalharga = float(totalharga)
 
     listconversion(listpesanan,2,float)
@@ -209,15 +275,20 @@ def menuawal(): #display awal (jihan)
     ==================================================""")
 
 def main(): #alur program (faatih)
-    menuawal()
-    pilihan = int(input(">> "))
-    if pilihan == 1 :
-        menupesan()
-    elif pilihan == 2 :
-        menucek()
-    elif pilihan == 3 :
-        menuexit()
-    else :
+    try:
+        global no
+        no = 0
+        menuawal()
+        pilihan = int(input(">> "))
+        if pilihan == 1 :
+            menupesan()
+        elif pilihan == 2 :
+            menucek()
+        elif pilihan == 3 :
+            menuexit()
+        else :
+            menuawal()
+    except:
         menuawal()
 
 main()
